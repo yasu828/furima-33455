fe7d76de19ed619e0d81ffcf4005512143c6272c
yasu828 <69378134+yasu828@users.noreply.github.com>
2021-01-18 19:13:12 +0900

    ユーザー管理機能の単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :fname, presence: true
-  validates :lname, presence: true
-  validates :fkata, presence: true
-  validates :lkata, presence: true
+  validates :fname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :lname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :fkata, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :lkata, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth, presence: true
-       
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
