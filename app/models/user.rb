f3287880039a95eb701c50847f113964f467035b
yasu828 <69378134+yasu828@users.noreply.github.com>
2021-01-18 13:43:28 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :fname, presence: true
+  validates :lname, presence: true
+  validates :fkata, presence: true
+  validates :lkata, presence: true
+  validates :birth, presence: true
+       
 end
