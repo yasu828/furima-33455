0945d5479ccca95dece4abcd329c9cd1a30e797a
yasu828 <69378134+yasu828@users.noreply.github.com>
2021-01-19 14:39:32 +0900

    バリデーションの記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,27 @@
 class Product < ApplicationRecord
+    belongs_to :user
+
+    with_options presence: true do
+        validates :product
+        validates :explain
+        validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+    end
+
+    # ActiveHashのアソシエーションとバリデーション
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :category
     belongs_to :state
     belongs_to :shipping
     belongs_to :prefecture
     belongs_to :waitday
+
+    with_options numericality: { other_than: 1 } do
+        validates :category_id
+        validates :state_id
+        validates :shipping_id
+        validates :prefecture_id
+        validates :waitday_id
+    end
+    # //ActiveHashのアソシエーションとバリデーション
 
 end
