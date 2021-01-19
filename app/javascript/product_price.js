3f9eac0b0022774e549c79cb0e9e81e42eee0cac
yasu828 <69378134+yasu828@users.noreply.github.com>
2021-01-19 19:18:50 +0900

    jsファイルの作成、販売価格表示

---
+++ app/javascript/product_price.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+    const productPrice = document.getElementById("item-price");
+    productPrice.addEventListener("input", () => {
+        const inputValue = productPrice.value;
+        const tax = 10;
+        const addTaxDom = document.getElementById("add-tax-price");
+        addTaxDom.innerHTML = inputValue / tax
+        const profit = document.getElementById("profit")
+        profit.innerHTML = inputValue - inputValue / tax
+    })
+
+});
