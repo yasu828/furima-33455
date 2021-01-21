3955fb63b0b03798551f94b2e29c43c4ed7030ed
yasu828 <69378134+yasu828@users.noreply.github.com>
2021-01-22 00:17:30 +0900

    コントローラーの記述

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,7 +19,7 @@ class ProductsController < ApplicationController
     end
 
     def edit
-        # @product = Product.find(params[:id])
+        @product = Product.find(params[:id])
     end
     
     def show
