9274cc7f40a40f35e2a8f4079297a414b05347ae
yasu828 <69378134+yasu828@users.noreply.github.com>
2021-01-21 17:12:06 +0900

    商品一覧表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
 
     def index
-        @products = Product.all.order(created_at: :desc)
+        @products = Product.all
     end
 
     def new
