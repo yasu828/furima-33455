d6c0aca84d5c6320b31f443f32ed0f295f5f0691
yasu828 <69378134+yasu828@users.noreply.github.com>
2021-01-21 19:08:44 +0900

    商品詳細を表示させる

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,9 +19,11 @@ class ProductsController < ApplicationController
     end
 
     def edit
+        # @product = Product.find(params[:id])
     end
     
     def show
+        @product = Product.find(params[:id])
     end
 
 
