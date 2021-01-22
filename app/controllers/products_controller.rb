0b0523db54dcdbd2ec16dc7f36527437f5ca35fe
yasu828 <69378134+yasu828@users.noreply.github.com>
2021-01-22 10:43:17 +0900

    商品編集機能の登録

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,15 @@ class ProductsController < ApplicationController
     def edit
         @product = Product.find(params[:id])
     end
+
+    def update
+        @product = Product.find(params[:id])
+        if @product.update(product_params)
+            redirect_to  product_path(@product.id)
+        else
+            render :edit
+        end
+    end
     
     def show
         @product = Product.find(params[:id])
