9e0337188e5ea8308c52245d2cad62c2b8635463
yasu828 <69378134+yasu828@users.noreply.github.com>
2021-01-31 16:46:41 +0900

    不要な記述の削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ require("@rails/activestorage").start()
 require("channels")
 require("../address")
 require("../product_price")
-require("../soldout")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- spec/models/address_buydatum_spec.rb
+++ spec/models/address_buydatum_spec.rb
@@ -3,91 +3,90 @@ require 'rails_helper'
 RSpec.describe AddressBuydatum, type: :model do
   before do
     @address_buydatum = FactoryBot.build(:address_buydatum)
-    @s = @address_buydatum
   end
 
   it "空欄全てに記述があれば登録できること" do
-    expect(@s).to be_valid
+    expect(@address_buydatum).to be_valid
   end
   
   
   it "郵便番号が必須であること" do
-    @s.postalcode = ''
-    @s.valid?
-    expect(@s.errors.full_messages).to include("Postalcode can't be blank")
+    @address_buydatum.postalcode = ''
+    @address_buydatum.valid?
+    expect(@address_buydatum.errors.full_messages).to include("Postalcode can't be blank")
   end
 
   it "都道府県が必須であること(1 = --)" do
-    @s.prefecture_id = '1'
-    @s.valid?
-    expect(@s.errors.full_messages).to include("Prefecture must be other than 1")
+    @address_buydatum.prefecture_id = 1
+    @address_buydatum.valid?
+    expect(@address_buydatum.errors.full_messages).to include("Prefecture must be other than 1")
   end
   
   it "市町村が必須であること" do
-    @s.addtwo = ''
-    @s.valid?
-    expect(@s.errors.full_messages).to include("Addtwo can't be blank")
+    @address_buydatum.addtwo = ''
+    @address_buydatum.valid?
+    expect(@address_buydatum.errors.full_messages).to include("Addtwo can't be blank")
   end
 
   it "番地が必須であること" do
-    @s.addthree = ''
-    @s.valid?
-    expect(@s.errors.full_messages).to include("Addthree can't be blank")
+    @address_buydatum.addthree = ''
+    @address_buydatum.valid?
+    expect(@address_buydatum.errors.full_messages).to include("Addthree can't be blank")
   end
 
   it "建物名は任意であること" do
-    @s.building = ''
-    expect(@s).to be_valid
+    @address_buydatum.building = ''
+    expect(@address_buydatum).to be_valid
   end
 
   it "電話番号が必須であること" do
-    @s.tel = ''
-    @s.valid?
-    expect(@s.errors.full_messages).to include("Tel can't be blank")
+    @address_buydatum.tel = ''
+    @address_buydatum.valid?
+    expect(@address_buydatum.errors.full_messages).to include("Tel can't be blank")
   end
 
 
   it "郵便番号にハイフンが必須であること" do
-    @s.postalcode = '1234567'
-    @s.valid?
-    expect(@s.errors.full_messages).to include("Postalcode is invalid")
+    @address_buydatum.postalcode = '1234567'
+    @address_buydatum.valid?
+    expect(@address_buydatum.errors.full_messages).to include("Postalcode is invalid")
   end
 
   it "郵便番号がハイフン込みの7文字であること" do
-    @s.postalcode = '123-456'
-    @s.valid?
-    expect(@s.errors.full_messages).to include("Postalcode is invalid")
+    @address_buydatum.postalcode = '123-456'
+    @address_buydatum.valid?
+    expect(@address_buydatum.errors.full_messages).to include("Postalcode is invalid")
   end
   
   it "郵便番号が半角数字の7文字であること" do
-    @s.postalcode = '１２３-４５６７'
-    @s.valid?
-    expect(@s.errors.full_messages).to include("Postalcode is invalid")
+    @address_buydatum.postalcode = '１２３-４５６７'
+    @address_buydatum.valid?
+    expect(@address_buydatum.errors.full_messages).to include("Postalcode is invalid")
   end
   
   it "郵便番号が半角数字の7文字であること" do
-    @s.postalcode = 'aaa-aaaa'
-    @s.valid?
-    expect(@s.errors.full_messages).to include("Postalcode is invalid")
+    @address_buydatum.postalcode = 'aaa-aaaa'
+    @address_buydatum.valid?
+    expect(@address_buydatum.errors.full_messages).to include("Postalcode is invalid")
   end
 
 
   it "電話番号は6〜11文字であること(11文字までしか入力できない)" do
-    @s.tel = '12'
-    @s.valid?
-    expect(@s.errors.full_messages).to include("Tel is invalid")
+    @address_buydatum.tel = '12'
+    @address_buydatum.valid?
+    expect(@address_buydatum.errors.full_messages).to include("Tel is invalid")
   end
 
   it "電話番号は半角数字でなければならないこと" do
-    @s.tel = '１２３４５６７８９０１'
-    @s.valid?
-    expect(@s.errors.full_messages).to include("Tel is invalid")
+    @address_buydatum.tel = '１２３４５６７８９０１'
+    @address_buydatum.valid?
+    expect(@address_buydatum.errors.full_messages).to include("Tel is invalid")
   end
 
   it "電話番号は半角数字でなければならないこと" do
-    @s.tel = 'aaaaggggfff'
-    @s.valid?
-    expect(@s.errors.full_messages).to include("Tel is invalid")
+    @address_buydatum.tel = 'aaaaggggfff'
+    @address_buydatum.valid?
+    expect(@address_buydatum.errors.full_messages).to include("Tel is invalid")
   end
 
 end
